f05573dd619cb10837edca032d1a0de7cb9e11cb
Jose Eneas <ninho.serafim95@gmail.com>
2021-12-14 11:03:55 -0300

    Alteraçao

--- src/nodeMailer/configs.ts
+++ src/nodeMailer/configs.ts
@@ -5,7 +5,7 @@ class SMTP_CONFIG {
     public host = process.env.EMAIL_HOST;
     public port = parseInt(process.env.EMAIL_HOST);
     public user = process.env.EMAIL_HOST;
-    public password = "3n34s1302";
+    public password = process.env.EMAIL_PASSWORD;
 }
 
 export default new SMTP_CONFIG();
